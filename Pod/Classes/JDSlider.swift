fca880ee7fcb714f9b45fe8cc0d4e4bcf50ecf20
Gonzalo Lopez <gslopez@uc.cl>
2016-07-19 17:27:32 -0400

    [UPDATED] public pageControl

--- Pod/Classes/JDSlider.swift
+++ Pod/Classes/JDSlider.swift
@@ -38,7 +38,7 @@ public class JDSliderView: UIView {
     //MARK:Private Properties
     private var numbersOfSlides    : Int = 0
     private var jdSliderScrollView : UIScrollView!
-    private var pageControl        : UIPageControl!
+    var pageControl                : UIPageControl!
 
     
     //MARK: Init
